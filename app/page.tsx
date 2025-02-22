8e47349079ef236e0225dd349841f01af67f3d16
Shad <Shadifmg@gmail.com>
2025-02-22 15:34:19 -0600

    Feat: Added headlessui dependency, and transferred Navbar component. Injected Navbar into app

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import { Author } from "./components/Author";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { Introduction } from "./components/Introdruction";
+import { NavBar } from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Introduction />
+      <NavBar />
       <Author />
       <Footer />
     </>

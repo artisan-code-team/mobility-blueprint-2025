a81db17b82b8d8536e948fc01813f850c5b6034a
Shad <Shadifmg@gmail.com>
2025-02-22 15:23:09 -0600

    Feat: Transfered Container, CheckIcon and Introduction Components. Injected Introduction Component into app.

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@
 import { Author } from "./components/Author";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
+import { Introduction } from "./components/Introdruction";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Introduction />
       <Author />
       <Footer />
     </>

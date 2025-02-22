4c488dcad42c2b321fe4ad557951386f7e9e5bba
Shad <Shadifmg@gmail.com>
2025-02-21 23:03:08 -0600

    Feat: Created Author component and added into the app.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-'use client'
+"use client";
 
-import { Hero } from "./components/Hero"
-
+import { Author } from "./components/Author";
+import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
-    <Hero />
-  )
+    <>
+      <Hero />
+      <Author />
+    </>
+  );
 }

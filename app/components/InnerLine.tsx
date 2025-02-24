d79eae950c45c4eabfa6db0a340b8cbd4b9b4c1a
Shad <Shadifmg@gmail.com>
2025-02-24 12:36:14 -0600

    Feat: Transferred Back Line component.

--- app/components/InnerLine.tsx
+++ app/components/InnerLine.tsx
@@ -36,21 +36,21 @@ import { useRouter } from "next/navigation";
 //   },
 // ]
 
-function PlayIcon(props: React.ComponentPropsWithoutRef<"svg">) {
-  return (
-    <svg
-      aria-hidden="true"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      viewBox="0 0 16 16"
-      {...props}
-    >
-      <path d="M6.75 10.25v-4.5L10.25 8l-3.5 2.25Z" />
-      <circle cx="8" cy="8" r="6.25" fill="none" />
-    </svg>
-  );
-}
+// function PlayIcon(props: React.ComponentPropsWithoutRef<"svg">) {
+//   return (
+//     <svg
+//       aria-hidden="true"
+//       strokeWidth="1.5"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       viewBox="0 0 16 16"
+//       {...props}
+//     >
+//       <path d="M6.75 10.25v-4.5L10.25 8l-3.5 2.25Z" />
+//       <circle cx="8" cy="8" r="6.25" fill="none" />
+//     </svg>
+//   );
+// }
 
 export function InnerLines() {
   const router = useRouter();
